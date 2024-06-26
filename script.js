d71f1656fd54eacda4b560f3248a6610610525e8
rhythmicrenai <165713237+rhythmicrenai@users.noreply.github.com>
2024-06-25 17:50:39 -0700

    menu bar for phone

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-
-// hello world!
+function myFunction() {
+    var x = document.getElementById("topnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+} 
